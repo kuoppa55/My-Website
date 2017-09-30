6cbd746cb3b55b6fd7d860cd259bf1e650558168
James Kuoppala <kuoppala@wisc.edu>
2017-09-30 17:03:40 -0500

    more javascript
    Messing around and styling

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-//var surname = prompt("Greetings friend, may I enquire as to your surname?");
-var apples = 5;
-var pears = 10;
-var piecesOfFruit = apples + pears;
-var piecesForEachPerson = piecesOfFruit / 3;
-
-var add = function(a, b)
-{
-	return a + b;
-	
-}
-
-var result = add(1,2);
-var jedi = {
-	name: "Yoda",
-	age: 899
-	
-};
-
-var jediName = jedi.name;
-alert("The best Jedi is " + jediName);
-jedi.lightsaber = "green";
-
-var emptyArray = [];
-var shoppingList = ["Milk", "Bread", "Beans"];
-alert(shoppingList[0]);
-var handleClick = function (event)
-{
-		alert("CLICKED");
-};
-var pageHeader = document.querySelector('#header');
-pageHeader.addEventListener('click', handleClick);
+$(document).ready(function() {
+    $("#trigger").click(function() {
+    alert("Button was clicked!");
+    });
+});
